49b0920e854ec3ec8a959946248b6350e2d475ba
patil215 <neilpatil215@gmail.com>
2018-01-15 15:56:28 -0600

    Write out rocket strategy

--- niels-java/RocketController.java
+++ niels-java/RocketController.java
@@ -1,5 +1,13 @@
 /**
- * Created by patil215 on 1/15/18.
+ * General strategy:
+ *
+ - Past round 400, start building rockets
+ - Rule: only build rockets if we have X number of factories being maintained, otherwise build factories
+ - Once (round# < 500 && #rockets > 0):
+ - Knowing rocket locations + capacity, pick X closest units (Maybe a little bigger than X in case some die) and tell them to run to rocket
+ - All rockets take off at turn 749 lmao
+ - Pick an axis, dump some rockets on one side, some on the other side
  */
 public class RocketController {
+
 }
