cfaf92c5f4f39af841ab8f51960a0676d16471f7
Niels <nielsskornerup@gmail.com>
2018-01-28 03:10:43 -0600

    fixed bug caused by merge

--- niels-java/Constants.java
+++ niels-java/Constants.java
@@ -24,7 +24,7 @@ public class Constants {
 	public static final int REACHABLE_KARBONITE_THREASHOLD = 120;
 	public static final int AMOUNT_STUCK_BEFORE_KILL = 10;
 
-	public static int LIMIT_WORKER_REP_ROUND = 300;
+	public static int LIMIT_WORKER_REP_ROUND = 50;
 
 	public static int ROCKET_LAUNCH_INTERVAL = 1;
 	public static int SAFE_FACTORY_DISTANCE = 4;
